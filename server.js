81408edbdecf5d5bbe2e16c5b1ce7764e55d997f
put your ghofranjdaradkh <Ghofran_Daradkh@hotmail.com>
2023-06-26 12:23:00 +0300

    lab13

--- server.js
+++ server.js
@@ -1,141 +1,52 @@
 `use strict`
 const express = require("express");
 const app = express();
+const pg = require("pg");
 
 let uniData = require("./ MovieData/data.json");
 const cors = require("cors");
 let axios = require("axios")
 require('dotenv').config();
 app.use(cors())
+app.use(express.json())
+// app.use(express.json()); 
 
-app.listen(3000, startingLog)
-function startingLog(req, res) {
-    console.log("Running at 3000")
-}
-
-
-//for Routes trending and search 
-let allinfo = [];
-function moviesLibrary(id, title, release_date, poster, overview) {
-    this.id = id
-    this.title = title,
-    this.release_date = release_date
-    this.poster_path = poster,
-    this.overview = overview,
-     allinfo.push(this)
-}
+const PORT = process.env.PORT;
+const DB_URL=process.env.DATABASE_URL;
 
 
-//for routes discover , topRated
-let newInf = [];
-function newMovieData(id, title, vote_average,adult,original_language ) {
-    this.id = id
-    this.title = title,
-    this.vote_average =vote_average
-    this.adult=adult,
-    this.original_language=original_language,
-     newInf.push(this)
-}
-//Routes
-app.get('/', handleData)
-app.get('/favorite', handleFavorite)
+const client =new pg.Client(DB_URL)
+client .connect().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Listening at ${PORT}`)
+       });
+})
 
-app.get('/trending', async (req, res) => {
 
-    let axiosRes = await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}&language=en-US`)
-    const allMovieInfo = [];
-    for (let i = 0; i < axiosRes.data.results.length; i++) {
-        const data = axiosRes.data.results[i];
-        const movieInfo = new moviesLibrary(data.id, data.title, data.release_date, data.poster_path, data.overview
-        )
-        allMovieInfo.push(movieInfo)
-    }
-    res.send(allMovieInfo)
+app.get('/',(req,res)=>{res.status(200).send(`main route `)})
+
+app.post("/addMovie", (req, res) => {
+    const { title ,author,release_date, original_language, Genre, overview ,Main_cast, Awards} = req.body;
+    let sql = `INSERT INTO movies ( title,author,release_date, original_language, Genre, overview ,Main_cast, Awards) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`;
+    client.query(sql, [title ,author,release_date, original_language, Genre, overview ,Main_cast, Awards]).then(() => {
+  res.status(201).send(`Movie ${title} added to database if you want to see go to route /getMovies `);
 });
-
-
-
-app.get('/search', async (req, res) => {
-    const movieName = req.query.name
-    let axiosRes2 = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${movieName}&page=2`)
-    const allMovieInfo = [];
-    for (let i = 0; i < axiosRes2.data.results.length; i++) {
-        const data = axiosRes2.data.results[i];
-        const movieInfo = new moviesLibrary(data.id, data.title, data.release_date, data.poster_path, data.overview
-        )
-        allMovieInfo.push(movieInfo)
-    }
-    res.send(allMovieInfo)
 });
+app.get("/getMovies",(req, res) => {
+    let sql = `SELECT * FROM movies`;
+    client.query(sql).then((moviesData) => {
+      res.status(200).send(moviesData.rows);
+    });
+
+})
 
 
 
-app.get('/discover',async (req, res) => {
-   
-    let axiosRes5 = await axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${process.env.API_KEY}`);
-    const newMovieInfo = [];
-        for (let i = 0; i < axiosRes5.data.results.length; i++) {
-            const data = axiosRes5.data.results[i];
-            
-            const movieInfo = new newMovieData(data.id, data.title, data.vote_average,data.original_language, data.adult)
-            
-            newMovieInfo.push(movieInfo)
-        }
-        res.send(newMovieInfo)
 
+//to handle error
 
-
-    
-});
-
-app.get('/topRated',async (req, res) => {
-
-        let axiosRes6 = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${process.env.API_KEY}`)
-        const newMovieInfo = [];
-            for (let i = 0; i < axiosRes6.data.results.length; i++) {
-                const data = axiosRes6.data.results[i];
-                
-                const movieInfo = new newMovieData(data.id, data.title, data.vote_average,data.original_language, data.adult)
-                
-                newMovieInfo.push(movieInfo)
-            }
-            res.send(newMovieInfo)
-        });
-        
-
-
-    
-   //new extra route  
-app.get('/languages',async (req, res) => {
-   
-    let axiosRes4 = await axios.get(`https://api.themoviedb.org/3/configuration/languages?api_key=${process.env.API_KEY}`);
-    res.send(axiosRes4.data)
-});
-    
 app.use(notFoundHandler)
 app.use(errorHandler);
-
-
-function handleData(req, res) {
-
-    try {
-        const movie = new moviesLibrary(uniData.title, uniData.poster_path, uniData.overview);
-        // Send the instance as a JSON response
-        res.json(movie);
-    }
-
-    catch (error) {
-        errorHandler(error, req, res, next)
-
-    }
-}
-
-
-function handleFavorite(req, res) {
-    res.send("Welcome to Favorite Page")
-
-}
-
 
 function notFoundHandler(req, res) {
     res.status(404).send({

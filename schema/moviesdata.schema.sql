8b824023bf4b42a5c3221d2becb1ab1f96d7077a
put your ghofranjdaradkh <Ghofran_Daradkh@hotmail.com>
2023-07-05 12:11:30 +0300

    DeploymentOnRender

--- schema/moviesdata.schema.sql
+++ schema/moviesdata.schema.sql
@@ -2,7 +2,6 @@ create table   if not exists moviesdata(
   id serial primary key,
 title varchar(200),
  release_date date,
-  original_language varchar(200),
   overview varchar(1000),
   comment varchar(300)
 );
